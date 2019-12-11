7576e6412316dbd01b67d12e33df6cac66fe3529
GenKawamura <gen.kawamura@cern.ch>
2019-12-11 15:42:34 +0100

    Updated

--- setup-htcondor.sh
+++ setup-htcondor.sh
@@ -1,4 +1,18 @@
 
+[ $# -eq 0 ] && echo "$0 [ui|wn]" && exit 0
+
+case $1 in
+    ui)
+	TYPE=ui
+	;;
+    wn)
+	TYPE=wn
+	;;
+    *)
+	echo "set ui or wn"
+	exit -1
+esac
+
 
 version=$(echo $OS_VERSION | perl -pe "s/^(.).*/\1/g")
 [ -z "$version" ] && . /etc/os-release && version=$VERSION_ID
@@ -10,3 +24,9 @@ rpm --import http://research.cs.wisc.edu/htcondor/yum/RPM-GPG-KEY-HTCondor
 
 yum -y --nogpgcheck install condor-procd condor-external-libs condor-bosco condor-classads condor-python condor
 
+## Copyting config
+cp -v config/condor_config.$TYPE /etc/condor/
+cp -v config/condor_ssh_to_job_sshd_config_template /etc/condor/
+cp -v config.d/* /etc/condor/config.d
+
+service condor restart
--- wn/200-configure-htcondor.sh
+++
@@ -1,3 +0,0 @@
-cp -v config/condor_config /etc/condor/
-cp -v config/condor_ssh_to_job_sshd_config_template /etc/condor/
-cp -v config.d/* /etc/condor/config.d
